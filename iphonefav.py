81a2d104e4a54c4603eb25fdb43e3f20e56a388b
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-09-13 22:53:43 +0200

    patched documentation

--- iphonefav.py
+++ iphonefav.py
@@ -1,3 +1,7 @@
+# Emanuele Ruffaldi, 2016
+#
+# See also this in C# uses a similar approach: https://github.com/geiszla/iOSLib/
+#
 import sqlite3
 import argparse
 import os
@@ -5,18 +9,10 @@ import shutil
 import datetime
 import touchexif
 
-#https://github.com/geiszla/iOSLib/wiki/ZGENERICASSET-contents
+#TODO album support
+#Albums Lookup many-many: ZGENERICASSET:Z_PK identified by Z_15ASSETS(Z_15ALBUMS,Z_22ASSETS)
+#MAYBE extra attribs: ZADDITIONALASSETATTRIBUTES
 
-#https://github.com/geiszla/iOSLib/blob/086e333e808bf49640fecb56cb65f7d16099a084/iOSLib/Internal/SQLite.cs
-# 1) ZGENERICASSET for list of photos
-# 2) ehance with additional attributes
-# 3) lookup for albums of photo
-#		SQLiteDataReader photoAlbumReader = makeRequest(sqlConnection, @"SELECT " + albumKeyName + " FROM " + device.Photos.assetTableName
-#                        + " WHERE " + device.Photos.assetKeyName + " = " + id);
-#		assetTableName = Z_15ASSETS
-#		assetKeyName = Z_15ALBUMS
-#		albumKeyName = Z_22ASSETS
-# 4) the id of the albums Z_15ASSETS can be solved using: SELECT ZTITLE FROM ZGENERICALBUM WHERE Z_PK
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Reports Favorites')
 	parser.add_argument('--path',nargs='+',help='path of photos')
@@ -27,30 +23,33 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
+	# create target
 	if args.target and not os.path.isdir(args.target):
 		os.mkdir(args.target)
+
 	print "paths:",args.path
 	print "db:",args.db
-	conn = sqlite3.connect(args.db)#detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+
+	conn = sqlite3.connect(args.db)
 	c = conn.cursor()
-	print "count ", [x[0] for x in c.execute("select count(*) from zgenericasset where zfavorite=1;")][0]
-	files = [x[0] for x in c.execute("select zfilename from zgenericasset where zfavorite=1;")]
+	files = [x for x in c.execute("select zfilename,zaddeddate from zgenericasset where zfavorite=1;")]
 	print "found,",len(files)
-	for f in files:
-		t = None
-		if t is not None:
-			t = datetime.datetime.fromtimestamp(t+978307200)
+	for f,t in files:
+		t = datetime.datetime.fromtimestamp(t+978307200) # 2001/1/1
 		found = None
+		# lookup somehwere
 		for p in args.path:
 			if os.path.isfile(os.path.join(p,f)):
 				found = os.path.join(p,f)
 				break
 		if found:
 			print "OK",found
+			# if target copy
 			if args.target:
 				tp = os.path.join(args.target,f)
 				if not os.path.isfile(tp) or args.overwrite:
 					shutil.copyfile(found,tp)
+				# if fix time modify timing
 				if t is not None and os.path.isfile(tp) and args.fixtime:
 					touchexif.touch(tp,t)
 		else:
